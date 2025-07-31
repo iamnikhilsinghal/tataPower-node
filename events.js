5fea1945d1cc0e6e93a217ecd584dd7b59baacbc
Nikhil Singhal <nikhilsinghal000@gmail..com>
2025-07-31 17:15:04 +0530

    async events

--- events.js
+++ events.js
@@ -9,6 +9,30 @@ emitter.on("test", () => {
 //   console.log("this is listener 2");
 // });
 
+// emitter.on("test", async () => {
+//   console.log("Listener started");
+//   await new Promise((resolve) => setTimeout(resolve, 1000));
+//   console.log("This is Listener 1 (done)");
+// });
+
+// emitter.on("test", () => {
+//   setTimeout(() => {
+//     console.log("this is settimeout");
+//   }, 1000);
+// });
+
+emitter.on("test", () => {
+  setTimeout(() => {
+    console.log("this is settimeout");
+  }, 1000);
+});
+
 console.log("Before emitting");
 emitter.emit("test");
 console.log("After emitting");
+
+
+// tasks-
+// order place ecommerce
+
+// add to cart
